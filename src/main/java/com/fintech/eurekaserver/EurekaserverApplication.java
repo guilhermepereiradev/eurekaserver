4d8cc4a514a6f2da242812e2622cf760e44a5ed1
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2024-02-06 20:37:54 -0300

    atualizando projeto

--- src/main/java/com/fintech/eurekaserver/EurekaserverApplication.java
+++ src/main/java/com/fintech/eurekaserver/EurekaserverApplication.java
@@ -3,6 +3,11 @@ package com.fintech.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +17,11 @@ public class EurekaserverApplication {
         SpringApplication.run(EurekaserverApplication.class, args);
     }
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+//                .httpBasic(withDefaults());
+//        http.csrf(csrf -> csrf.ignoringRequestMatchers("/eureka/**"));
+//        return http.build();
+//    }
 }
---
+++ src/main/java/com/fintech/eurekaserver/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.fintech.eurekaserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authz -> authz
+//                        .anyRequest()
+//                        .authenticated())
+                .httpBasic(withDefaults())
+                .build();
+
+    }
+
+}
